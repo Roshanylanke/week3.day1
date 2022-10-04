fc229ea9e6875ad7afafd52715954c2ee41a49f1
Roshanylanke <113982181+Roshanylanke@users.noreply.github.com>
2022-10-04 13:07:59 +0530

    Add files via upload

---
+++ methodOverriding/BankInfo.java
@@ -0,0 +1,21 @@
+package methodOverriding;
+
+public class BankInfo {
+	    
+	    // Class     :AxisBank
+	     //Methods :deposit()
+
+	
+	//You have to override the method deposit in AxisBank.
+	public void saving() {
+	    	System.out.println("Savings Account");
+	    }
+	    public void fixed() {
+	    	System.out.println("Fixed Account");
+	    }
+	    public void deposit() {
+	    	System.out.println("Dp Account");
+	    }
+	
+	
+}
---
+++ org/system/Desktop.java
@@ -0,0 +1,20 @@
+package org.system;
+
+public class Desktop extends Computer{
+	
+	public void desktopSize() {
+		System.out.println("size of desktop");
+	}
+	
+	public static void main(String[] args) {
+		Desktop ds = new Desktop();
+		ds.desktopSize();
+		ds.computerModel();
+	
+		
+	}
+		
+		
+	}
+
+
